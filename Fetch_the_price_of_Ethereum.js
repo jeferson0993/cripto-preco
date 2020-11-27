b619113690680fac4255e609047236ee4457506d
Jeferson Ferreira <jeferson0993@gmail.com>
2020-11-27 16:10:28 -0300

    Add files via upload

---
+++ Fetch_the_price_of_Ethereum.js
@@ -0,0 +1,21 @@
+//Fetch the price of Ethereum
+const eth_api_url = 'https://api.cryptonator.com/api/ticker/eth-brl';
+function ethereumHttpObject() {
+    try { return new XMLHttpRequest(); }
+    catch (error) { console.error(error.message) }
+}
+function ethereumGetData() {
+    var request = ethereumHttpObject();
+    request.open("GET", eth_api_url, false);
+    request.send(null);
+    // console.log(request.responseText);
+    return request.responseText;
+}
+function ethereumDataHandler() {
+    var raw_data_string = ethereumGetData();
+    var data = JSON.parse(raw_data_string);
+    var price = data.ticker.price;
+    // console.log({price});
+    document.getElementById("eth_val").innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(price);
+    return price;
+}
---
+++ app.js
@@ -0,0 +1,43 @@
+document
+.getElementsByClassName("about-menu")[0]
+.addEventListener("click", () => {
+    document
+    .getElementById("content").style.display = "none";
+    document
+    .getElementById("about").style.display = "";
+}, false);
+
+document
+.getElementsByClassName("bitcoin-menu")[0]
+.addEventListener("click", () => {
+    document
+    .getElementById("about").style.display = "none";
+    document
+    .getElementById("content").style.display = "";
+}, false);
+
+document
+.getElementById("Open-user-menu")
+.addEventListener("click", () => {
+    (() => {
+        var dropdown_menu = document.getElementById("dropdown-menu");
+        if (dropdown_menu.style.display === "none") {
+            dropdown_menu.style.display = "block";
+        } else {
+            dropdown_menu.style.display = "none";
+        }
+    })();
+});
+
+document
+.getElementById("mobile-menu-button")
+.addEventListener("click", () => {
+    (() => {
+        var mobile_menu = document.getElementById("mobile-menu");
+        if (mobile_menu.style.display === "none") {
+            mobile_menu.style.display = "block";
+        } else {
+            mobile_menu.style.display = "none";
+        }
+    })();
+});
