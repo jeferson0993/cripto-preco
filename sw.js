0f940aba9d51a5b2aafc3f5590d19c3ee37a8cc4
Jeferson Ferreira <jeferson0993@gmail.com>
2020-11-28 13:16:14 -0300

    update add to home screen

--- sw.js
+++ sw.js
@@ -1,44 +1,21 @@
-const STATIC_CACHE_NAME = "cripto-preto-1.0.0";
-const ASSETS = [
-  "/",
-  "./app.js",
-  "./index.js",
-  "./Fusion_charts_render.js",
-  "./Fetch_the_value_of_Bitcoin.js",
-
-  "./estados-unidos.svg",
-  "./brasil.svg"
-];
-
-// install event
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(STATIC_CACHE_NAME).then(function (cache) {
-      return cache.addAll(ASSETS);
-    })
-  );
- });
-
-// activate event
-self.addEventListener("activate", function (e) {
-  console.log("service worker %cactivated","color: green");
-  e.waitUntil(
-    caches.keys().then(keys => {
-      // console.log("keys: ", keys);
-      return Promise.all(keys
-        .filter(key => key !== STATIC_CACHE_NAME)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('cripto-preco').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/index.js',
+       '/app.js'
+     ]);
+   })
+ );
 });
 
-// fetch event
-self.addEventListener("fetch", function (e) {
-  // console.log("fetch event", e);
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
   e.respondWith(
-    caches.match(e.request).then(cacheRes => {
-      return cacheRes || fetch(e.request);
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
