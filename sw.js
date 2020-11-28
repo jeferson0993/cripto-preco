89dc381cd7633ee09dbd8aa9805e126c55b96499
Jeferson Ferreira <jeferson0993@gmail.com>
2020-11-28 15:30:13 -0300

    update service work file

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
-self.addEventListener('install', function(e) {
+self.addEventListener("install", function(e) {
  e.waitUntil(
-   caches.open('cripto-preco').then(function(cache) {
+   caches.open("cripto-preco").then(function(cache) {
      return cache.addAll([
-       '/',
-       '/index.html',
-       '/index.js',
-       '/app.js'
+       "/",
+       "/index.html",
+       "/index.js",
+       "/app.js"
      ]);
    })
  );
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener("fetch", function(e) {
   console.log(e.request.url);
   e.respondWith(
     caches.match(e.request).then(function(response) {
