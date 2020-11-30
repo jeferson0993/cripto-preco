b810c8341cc77a295af1ccd2341a9b3511208102
Jeferson Ferreira <jeferson0993@gmail.com>
2020-11-30 10:43:33 -0300

    atualizado service work para atulizacao automatica por versao

--- app.js
+++ app.js
@@ -127,10 +127,10 @@ function renderWallets(wallets) {
         element.setAttribute('data-key', item.id);
 
         element.innerHTML = `    
-    <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8 rounded-md bg-indigo-800">
         <div class="flex items-center justify-between flex-wrap">
         <div class="w-0 flex-1 flex items-center">
-            <span class="flex p-2 rounded-lg bg-indigo-800">
+            <span class="flex p-2 rounded-lg">
             <!-- Heroicon name: speakerphone -->
             <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                 stroke="currentColor" aria-hidden="true">
--- cripto-preco-service-worker.js
+++ cripto-preco-service-worker.js
@@ -1,25 +1,42 @@
-self.addEventListener("install", function(e) {
- e.waitUntil(
-   caches.open("cripto-preco").then(function(cache) {
-     return cache.addAll([
-       "/cripto-preco/",
-       "/cripto-preco/app.js",
-       "/cripto-preco/index.js",
-       "/cripto-preco/index.html",
-       "/cripto-preco/brasil.svg",
-       "/cripto-preco/estados-unidos.svg",
-       "/cripto-preco/Fusion_charts_render.js",
-       "/cripto-preco/Fetch_the_value_of_Bitcoin.js",
-     ]);
-   })
- );
+const STATIC_CACHE_NAME = "cripto-preco-v1.0.2";
+const ASSETS = [
+  "/cripto-preco/",
+  "/cripto-preco/app.js",
+  "/cripto-preco/index.js",
+  "/cripto-preco/index.html",
+  "/cripto-preco/brasil.svg",
+  "/cripto-preco/estados-unidos.svg",
+  "/cripto-preco/Fusion_charts_render.js",
+  "/cripto-preco/Fetch_the_value_of_Bitcoin.js"
+];
+
+// install event
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(STATIC_CACHE_NAME).then(function (cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-self.addEventListener("fetch", function(e) {
-  console.log(e.request.url);
+// activate event
+self.addEventListener("activate", function (e) {
+  console.log("service worker %cactivated!", "color: green");
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== STATIC_CACHE_NAME)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// fetch event
+self.addEventListener("fetch", function (e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+    caches.match(e.request).then(cacheRes => {
+      return cacheRes || fetch(e.request);
     })
   );
 });
